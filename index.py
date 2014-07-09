885d7bbd0500ce00f342e0fe13e06ad679069bb6
Christopher Nies <christophernies@gmail.com>
2014-07-08 23:28:35 -0400

    Added index2 with different layout to accomodate additional changes

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(name=None):
-    return render_template('index.html', name=name)
+    return render_template('index2.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
