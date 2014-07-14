256a6f04eb6bed3f8181c993613a7cd4ba7b87c6
Christopher <christophernies@gmail.com>
2014-07-13 21:51:08 -0400

    added index.py code to __init__.py (so that it actually works now). Also added routes for social media and apps.

--- __init__.py
+++ __init__.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html', name=name)
 
+@app.route('/feeds')
+def feeds():
+    return render_template('feeds.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
